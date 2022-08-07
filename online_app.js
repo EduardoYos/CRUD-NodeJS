4821709f0a7273bb8bbcfe61d7301366ec1f499f
Eduardo Yoshio <eduardo.yoshio@outlook.com>
2022-08-06 22:38:25 -0300

    up heroku app

--- online_app.js
+++ online_app.js
@@ -35,11 +35,62 @@ app.engine("handlebars",engine({defaultLayout:'main'}));
 app.set('view engine','handlebars');
 
 
+//Routes and Templates
+app.get("/",function(req,res){
+    /*res.send("Essa é minha página inicial!");*/
+     /*res.sendFile(__dirname+"/index.html");*/
+     /*console.log(req.params.id);*/
+     res.render('index');
+ });
+
+ app.get("/insert",function(req,res){
+    res.render("insert");
+});
+ app.get("/select/:id?",function(req,res){
+     if(!req.params.id){
+         sql.getConnection(function(err,connection){
+             connection.query("select * from user order by id asc",function(err,results,fields){
+                 res.render('select',{data:results});
+             });
+         });
+     }else{
+         sql.getConnection(function(err,connection){
+             connection.query("select * from user where id=? order by id asc",[req.params.id],function(err,results,fields){
+                 res.render('select',{data:results});
+             });
+         });
+     }
+ });
+ app.post("/controllerForm",urlencodeParser,function(req,res){
+     sql.getConnection(function(err,connection){
+         connection.query("insert into user values (?,?,?)",[req.body.id,req.body.name,req.body.age]);
+         res.render('controllerForm',{name:req.body.name});
+     });
+ });
+ app.get('/delete/:id',function(req,res){
+     sql.getConnection(function(err,connection){
+         connection.query("delete from user where id=?",[req.params.id]);
+         res.render('delete');
+     });
+ });
+ app.get("/update/:id",function(req,res){
+     sql.getConnection(function(err,connection){
+             connection.query("select * from user where id=?",[req.params.id],function(err,results,fields){
+             res.render('update',{id:req.params.id,name:results[0].name,age:results[0].age});
+         });
+     });
+ });
+ app.post("/controllerUpdate",urlencodeParser,function(req,res){
+     sql.getConnection(function(err,connection){
+         connection.query("update user set name=?,age=? where id=?",[req.body.name,req.body.age,req.body.id]);
+         res.render('controllerUpdate');
+     });
+ });
 
 //ROUTES
-app.get("/",function(req,res){
-   res.send('oi');
-});
+// app.get("/",function(req,res){
+//    res.send('oi');
+// });
 
 // app.get("/",function(req,res){
 //     res.render('index');
